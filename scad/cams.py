aa7cee3f51669f28a3a337a9938dc6c595414969
Jim MacArthur <jim@mode7.co.uk>
2023-02-25 13:17:03 +0000

    cams.py: Add OpenSCAD output

--- scad/cams.py
+++ scad/cams.py
@@ -30,6 +30,8 @@ events = [
     ]
 
 svg_output_filename = "timing.svg"
+openscad_output_filename = "cams.scad"
+openscad_time_step = 0.01
 
 # The first 8 cams (0-7) are instruction cams
 cams = {
@@ -46,13 +48,17 @@ cams = {
 
 def most_recent_value(timing_points, time):
     # Find the most recent value reached before time
-    points = [(point_time, value) for (point_time, value) in timing_points if point_time < time]
+    points = [(point_time, value) for (point_time, value) in timing_points if point_time <= time]
     sorted_points = sorted(points, key = lambda x: x[0])
     return (sorted_points[-1])
 
+def nearest_future_value(timing_points, time):
+    # Find the value of the cam at or after time
+    points = [(point_time, value) for (point_time, value) in timing_points if point_time >= time]
+    sorted_points = sorted(points, key = lambda x: x[0])
+    return (sorted_points[0])
 
 def process_cams(cams, events):
-
     cams_by_short_name = dict([(c.short_name, c) for c in cams.values()])
     for (index, cam) in cams.items():
         cam.index = index
@@ -80,6 +86,21 @@ def process_cams(cams, events):
     for c in cams.values():
         c.timing_points.append((1.0, c.timing_points[0][1]))
 
+def interpolate(cam, time):
+    # Find the right value of the cam at t...
+
+    (last_time, last_value) = most_recent_value(cam.timing_points, time)
+    (next_time, next_value) = nearest_future_value(cam.timing_points, time)
+
+    # If there's an exact match, just return that
+    if last_time == time:
+        return last_value
+    elif next_time == time:
+        return next_value
+    span = next_time - last_time
+    progress = (time - last_time)/span
+    return last_value + (next_value - last_value)*progress
+
 def write_svg(svg_filename, cams):
     with open(svg_filename, "w") as f:
         f.write("<svg>\n")
@@ -93,8 +114,16 @@ def write_svg(svg_filename, cams):
             f.write(f"<text x=\"-50\" y=\"{yoffset}\">{c.short_name}</text>\n")
             f.write(f"<text x=\"110\" y=\"{yoffset}\">{c.short_name}</text>\n")
         f.write("</svg>\n")
+
+def write_openscad(filename, cams):
+    with open(filename, "w") as f:
+        f.write("cam_levels = [\n")
+        for cam in cams.values():
+            no_steps = int(1/openscad_time_step)
+            steps = [ str(interpolate(cam, step*openscad_time_step)) for step in range(0,no_steps) ]
+            f.write("[" + ", ".join(steps) + "],\n")
+        f.write("]\n")
+
 process_cams(cams, events)
-
-
-
 write_svg(svg_output_filename, cams)
+write_openscad(openscad_output_filename, cams)
